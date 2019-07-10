b27762808885393830cd13a44b8d5fb0cbeecbb0
aleksandr <aleksandr1104nk@gmail.com>
2019-07-10 17:17:39 +0300

    2019-04-26

---
+++ src/Admin/ProductAdmin.php
@@ -0,0 +1,36 @@
+<?php
+namespace App\Admin;
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+class ProductAdmin extends AbstractAdmin
+{
+    protected function configureListFields(ListMapper $list)
+    {
+        $list->addIdentifier('name')
+//            ->add('categories')
+            ->addIdentifier('description')
+            ->add('price')
+            ->add('count')
+            ->add('isTop');
+    }
+    protected function configureDatagridFilters(DatagridMapper $filter)
+    {
+        $filter->add('name')
+//            ->add('categories')
+            ->add('description')
+            ->add('price')
+            ->add('count')
+            ->add('isTop');
+    }
+    protected function configureFormFields(FormMapper $form)
+    {
+        $form->add('name')
+//            ->add('categories')
+            ->add('description')
+            ->add('price')
+            ->add('count')
+            ->add('isTop');
+    }
+}
